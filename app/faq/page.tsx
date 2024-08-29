46fa2060b96c858839d50df1c06d41335388b6a3
Anna Viklund <annamariaviklund@gmail.com>
2024-08-29 16:41:59 +0200

    Update FAQ

--- app/faq/page.tsx
+++ app/faq/page.tsx
@@ -1,4 +1,4 @@
-import { FIELDGUIE_URL } from "@/lib/constants";
+import { FIELDGUIE_URL, ML_REPO_URL } from "@/lib/constants";
 import { ReactNode } from "react";
 
 export default function FAQ() {
@@ -11,11 +11,20 @@ export default function FAQ() {
           deepen our understanding of biodiversity and accelerate conservation
           efforts.
         </Question>
+        <Question title="What kind of invertebrates can you identify?">
+          Antenna currently offers machine learning models to recognise
+          nocturnal lepidoptera (moths). However the platform is flexible and
+          can support most detection or classification algorithms. Note that the
+          default processing pipeline is best suited for sequential (time
+          series) images that contain multiple insects. The organisms must be
+          visually identifiable from the “above” perspective. See our{" "}
+          <a href={ML_REPO_URL}>Github repository</a> for more information.
+        </Question>
         <Question title="How can I get and set up a monitoring station?">
           The Platform is device-agnostic. You can buy and set up any type of
           camera, then log on to the Platform to upload and process your images.
         </Question>
-        <Question title="How can I get and set up a monitoring station?">
+        <Question title="Can I use the Platform even though I am not a researcher?">
           We LOVE citizen scientists! You absolutely can use the public dataset.
           However we are not quite ready to receive images from phone cameras.
           Until we are, you can check out our partner’s app{" "}
