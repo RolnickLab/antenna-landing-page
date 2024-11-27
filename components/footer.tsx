f54c7a420517b80f0011ee37c0d03c14c46a2231
Anna Viklund <annamariaviklund@gmail.com>
2024-11-27 12:48:23 +0100

    Update footer layout

--- components/footer.tsx
+++ components/footer.tsx
@@ -6,18 +6,16 @@ export const Footer = () => {
   const { theme } = useTheme();
 
   return (
-    <footer className="p-8 bg-muted border-t">
-      <div className="flex flex-col items-center justify-center gap-4">
-        <h1 className="text-sm font-medium uppercase text-muted-foreground">
-          Hosted by
-        </h1>
+    <footer className="p-4 bg-muted border-t">
+      <div className="max-w-screen-lg flex items-center justify-center gap-4 mx-auto">
+        <span className="text-sm text-muted-foreground">Hosted by</span>
         <a
           href="https://www.netlify.com/"
           target="_blank"
           rel="noopener noreferrer"
         >
           <img
-            className="h-16"
+            className="w-[100px]"
             src={
               theme === "light"
                 ? "/images/logo-netlify-dark.png"
