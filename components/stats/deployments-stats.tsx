a757ef6afd6d9aed6040215b0fe27567c4803e64
Anna Viklund <annamariaviklund@gmail.com>
2025-10-20 13:45:04 +0200

    chore: add clarifying comments

--- components/stats/deployments-stats.tsx
+++ components/stats/deployments-stats.tsx
@@ -8,11 +8,11 @@ export const DeploymentsStats = () => {
     <Stats
       items={[
         {
-          value: `${Math.round(stats.deployments_count / 100) * 100}`,
+          value: `${Math.round(stats.deployments_count / 100) * 100}`, // Round to nearest 100
           unit: "Stations",
         },
         {
-          value: `${Math.round(stats.captures_count / 1000000)}M`,
+          value: `${Math.round(stats.captures_count / 1000000)}M`, // Convert to millions
           unit: "Camera images",
         },
       ]}
